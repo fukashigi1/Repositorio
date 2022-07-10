f184340ba2663ea691e8b1efa75b7c8852d3094b
fukashigi <maatiaasbkn2@gmail.com>
2022-07-10 11:18:54 -0400

    10-07-2022

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ namespace Proyecto{
     class Program{
         static void Main(string[] args){
             Caca caca = new Caca();
+            Index2 index2 = new Index2();
+            Console.WriteLine(index2.Metodo2(3));
             caca.Metodo();
         }
     }
