a0a63a1914c1fc3d395be8be94e10b005f7092b9
fukashigi <maatiaasbkn2@gmail.com>
2022-07-10 11:13:25 -0400

    Creado el Main

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Proyecto{
+    class Program{
+        static void Main(string[] args){
+            Caca caca = new Caca();
+            caca.Metodo();
+        }
+    }
+}
